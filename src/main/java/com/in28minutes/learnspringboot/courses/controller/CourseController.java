7604210b08af2e0d3faef7d7d059d543dee6f134
radwanromy <radwanromy@gmail.com>
2021-11-15 13:06:13 +0600

    SBWA- Docker 004

--- src/main/java/com/in28minutes/learnspringboot/courses/controller/CourseController.java
+++ src/main/java/com/in28minutes/learnspringboot/courses/controller/CourseController.java
@@ -1,4 +1,5 @@
 package com.in28minutes.learnspringboot.courses.controller;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -12,7 +13,16 @@ import java.util.Arrays;
 public class CourseController {
     // http://localhost:8080/courses
     @GetMapping("/courses")
-    public List<Course> getAllCourses(){
-        return Arrays.asList(new Course(1L, "Learn Microservices","Radwan Romy") );
+    public List<Course> getAllCourses() {
+        return Arrays.asList(
+                new Course(1L, "Learn Microservices", "Radwan Romy"),
+                new Course(2L, "FCB", "Leonal Messi"),
+                new Course(3L, "La Masia", "Ansu Fati"),
+                        new Course(4L, "Physics", "Newton"),
+                        new Course(5L, "D.L", "Math"),
+                        new Course(6L, "SpaceX", "Elon Mask"),
+                                new Course(7L, "Apple", "Steven jobs"),
+                                new Course(8L, "Microsoft", "Bill gates"),
+                                new Course(9L, "Mobile", "Alegendar"));
     }
 }
